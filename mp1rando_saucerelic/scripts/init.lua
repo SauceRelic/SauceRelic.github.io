6ab8150c36473507c49690f390c7e39aee3bc7ad
SauceRelic <39061831+SauceRelic@users.noreply.github.com>
2018-10-10 10:09:09 -0500

    finished mp1 migration

---
+++ mp1rando_saucerelic/scripts/init.lua
@@ -0,0 +1,11 @@
+Tracker:AddItems("items/settings.json")
+Tracker:AddItems("items/equips.json")
+Tracker:AddItems("items/artifacts.json")
+
+Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/logic.json")
+Tracker:AddLocations("locations/locations.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")

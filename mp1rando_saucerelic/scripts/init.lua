567c8a95da9fab06f2e9ec7dddef791c90a4bc71
SauceRelic <39061831+SauceRelic@users.noreply.github.com>
2018-10-19 18:59:21 -0500

    added script inclusion for dev branch

--- mp1rando_saucerelic/scripts/init.lua
+++ mp1rando_saucerelic/scripts/init.lua
@@ -2,10 +2,16 @@ Tracker:AddItems("items/settings.json")
 Tracker:AddItems("items/equips.json")
 Tracker:AddItems("items/artifacts.json")
 
-Tracker:AddMaps("maps/maps.json")
+if not (string.find(Tracker.ActiveVariantUID, "v_itemsonly")) then
+  if string.find(Tracker.ActiveVariantUID, "v_dev")) then
+    ScriptHost:LoadScript("scripts/logic.lua")
+  end
+  
+  Tracker:AddMaps("maps/maps.json")
 
-Tracker:AddLocations("locations/logic.json")
-Tracker:AddLocations("locations/locations.json")
+  Tracker:AddLocations("locations/logic.json")
+  Tracker:AddLocations("locations/locations.json")
+end
 
 Tracker:AddLayouts("layouts/shared.json")
 Tracker:AddLayouts("layouts/tracker.json")

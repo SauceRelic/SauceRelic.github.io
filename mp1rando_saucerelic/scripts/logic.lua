622c1abde373cd60ec615480228bbcdae8df2b74
SauceRelic <fadingraider@gmail.com>
2018-10-23 18:02:13 -0500

    shortcut for item checks, included any suit for magmoor runs

--- mp1rando_saucerelic/scripts/logic.lua
+++ mp1rando_saucerelic/scripts/logic.lua
@@ -1,21 +1,33 @@
+function hasItem(item)
+  if Tracker:ProviderCountForCode(item) >= 1 then
+    return true
+  else
+    return false
+  end
+end
+
 function antiGravity()
-  if Tracker:ProviderCountForCode("gravity") > 0 then
+  if hasItem("gravity") then
     return 0
   else
     return 1
   end
 end
 
-function suitlessMagmoorRun()
-  if Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("vmretanks") then
+function MagmoorRun()
+  if hasItem("trick_vmr") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("vmretanks")) then
+    return 1
+  elseif hasItem("varia") or hasItem("gravity") or hasItem("phazon") then
     return 1
   else
     return 0
   end
 end
 
-function suitlessEarlyMagmoor()
-  if Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("earlyetanks") then
+function EarlyMagmoor()
+  if hasItem("trick_earlymagitems") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("earlyetanks")) then
+    return 1
+  elseif hasItem("varia") or hasItem("gravity") or hasItem("phazon") then
     return 1
   else
     return 0

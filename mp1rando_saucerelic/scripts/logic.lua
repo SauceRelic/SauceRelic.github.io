aff7a417626e9cd5eba9a403bfa60abf982c8ba2
SauceRelic <fadingraider@gmail.com>
2018-10-26 21:51:06 -0500

    added inverter for nolatebombs, reduced verbosity

--- mp1rando_saucerelic/scripts/logic.lua
+++ mp1rando_saucerelic/scripts/logic.lua
@@ -7,29 +7,25 @@ function hasItem(item)
 end
 
 function antiGravity()
-  if hasItem("gravity") then
-    return 0
-  else
-    return 1
-  end
+  return not hasItem("gravity")
+end
+
+function allowBackPhenBombs()
+  return not hasItem("nolatebombs")
 end
 
 function MagmoorRun()
   if hasItem("trick_vmr") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("vmretanks")) then
     return 1
-  elseif hasItem("varia") or hasItem("gravity") or hasItem("phazon") then
-    return 1
   else
-    return 0
+    return hasItem("varia") or hasItem("gravity") or hasItem("phazon")
   end
 end
 
 function EarlyMagmoor()
   if hasItem("trick_earlymagitems") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("earlyetanks")) then
     return 1
-  elseif hasItem("varia") or hasItem("gravity") or hasItem("phazon") then
-    return 1
   else
-    return 0
+    return hasItem("varia") or hasItem("gravity") or hasItem("phazon")
   end
 end

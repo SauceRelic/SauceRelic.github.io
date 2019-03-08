5b034e9f38bc7f9196aebe47b1d2b9f8134b83c1
SauceRelic <39061831+SauceRelic@users.noreply.github.com>
2019-03-08 14:49:59 -0600

    fixed auto placement, finished implementation

--- mp1rando_saucerelic/scripts/special.lua
+++ mp1rando_saucerelic/scripts/special.lua
@@ -10,7 +10,30 @@ function ArtifactCounter()
 end
 
 function ShuffleItems()
-  VanillaTrack("@Chozo Ruins/Hive Totem/Missile Launcher", "shuf_launcher", "missile")
+  -- Missile Launcher
+  VanillaTrack("@Hive Totem/Missile Launcher", "shuf_launcher", "missile")
+  -- Morph Ball
+  VanillaTrack("@Ruined Shrine/Beetle Battle (Morph Ball)", "shuf_morph", "morph")
+  -- Bombs
+  VanillaTrack("@Burn Dome/Incinerator Drone (Morph Bomb)", "shuf_bombs", "bomb")
+  -- Charge Beam
+  VanillaTrack("@Watery Hall/Scan Puzzle (Charge Beam)", "shuf_charge", "charge")
+  -- Space Jump
+  VanillaTrack("@Alcove/Space Jump", "shuf_space", "space")
+
+  -- Artifacts
+  VanillaTrack("@Artifact Temple/Artifact of Truth", "shuf_artifacts", "truth")
+  VanillaTrack("@Warrior Shrine/Artifact of Strength", "shuf_artifacts", "strength")
+  VanillaTrack("@Control Tower/Artifact of Elder", "shuf_artifacts", "elder")
+  VanillaTrack("@Sunchamber/Ghosts (Artifact of Wild)", "shuf_artifacts", "wild")
+  VanillaTrack("@Tower Chamber/Artifact of Lifegiver", "shuf_artifacts", "lifegiver")
+  VanillaTrack("@Elite Research/Phazon Elite (Artifact of Warrior)", "shuf_artifacts", "warrior")
+  VanillaTrack("@Life Grove/Artifact of Chozo", "shuf_artifacts", "chozo")
+  VanillaTrack("@Lava Lake/Artifact of Nature", "shuf_artifacts", "nature")
+  VanillaTrack("@Chozo Ice Temple/Artifact of Sun", "shuf_artifacts", "sun")
+  VanillaTrack("@Elder Chamber/Artifact of World", "shuf_artifacts", "world")
+  VanillaTrack("@Storage Cave/Artifact of Spirit", "shuf_artifacts", "spirit")
+  VanillaTrack("@Phazon Mining Tunnel/Artifact of Newborn", "shuf_artifacts", "newborn")
 end
 
 function VanillaTrack(locationref, setting, item)

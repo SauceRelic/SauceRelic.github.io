7739c1369136b5cfa0d561cb0fa6eb5cc74a474c
SauceRelic <fadingraider@gmail.com>
2020-08-28 21:16:32 -0500

    using lua more for access rule logic

--- mp1rando_saucerelic/scripts/logic.lua
+++ mp1rando_saucerelic/scripts/logic.lua
@@ -1,3 +1,4 @@
+-- basic functions
 function hasItem(item)
   if Tracker:ProviderCountForCode(item) >= 1 then
     return true
@@ -11,7 +12,7 @@ function missing(item)
 end
 
 function disabled(item,sb)
-  if sb == nil then
+  if not sb then
     return missing(item)
   else
     if missing(item) then
@@ -22,38 +23,89 @@ function disabled(item,sb)
   end
 end
 
-function MagmoorRun()
-  if hasItem("t_vmr") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("vmrtanks")) then
-    return true
-  elseif hasItem("varia") or (hasItem("hs_anysuit") and (hasItem("gravity") or hasItem("phazon"))) then
-    return true
-  elseif hasItem("t_vmr") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("sb_vmrtanks")) then
-    return true, AccessibilityLevel.SequenceBreak
-  elseif hasItem("sb_t_vmr") and ((Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("vmrtanks")) or (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("sb_vmrtanks"))) then
-    return true, AccessibilityLevel.SequenceBreak
+-- Evaluates a boolean statement consisting of only or operators between terms
+function orN(...)
+  for i,v in ipairs({...}) do
+    if hasItem(v) then
+      return true
+    end
+  end
+  return false
+end
+
+
+-- Evaluates a boolean statement with structure:
+-- (hasItem(x) and ... and hasItem(z)) or (...) or ...
+-- Arguments are either item code strings or "or", which denotes or operators in the structure
+function eval(...)
+  local statementPart = true
+
+  for i,v in ipairs({...}) do
+
+    if v == "or" then
+      if statementPart then
+        return true
+      else
+        statementPart = true
+      end
+
+    else
+      if statementPart then
+        statementPart = hasItem(v)
+      end
+
+    end
+  end
+
+  return statementPart
+end
+
+
+-- item combo requirements
+function canBomb()
+  return hasItem(morph) and hasItem(bomb)
+end
+
+function canPb()
+  return hasItem(morph) and hasItem(powerbomb)
+end
+
+function canBombOrPb()
+  return hasItem(morph) and (hasItem(bomb) or hasItem(powerbomb))
+end
+
+function canBoost()
+  return hasItem(morph) and hasItem(boost)
+end
+
+function canSpider()
+  return hasItem(morph) and hasItem(spider)
+end
+
+function canSuper()
+  return hasItem(missile) and hasItem(charge) and hasItem(super)
+end
+
+function thermalReqs()
+  return hasItem(thermal) or hasItem(t_removeThermalReqs)
+end
+
+function xrayReqs()
+  return hasItem(xray) or hasItem(t_removeXrayReqs)
+end
+
+function hasItemSuit()
+  if hasItem(hp_variaonly) then
+    return hasItem(varia)
   else
-    return false
+    return hasItem(varia) or hasItem(gravity) or hasItem(phazon)
   end
 end
 
-function EarlyMagmoor()
-  if hasItem("t_earlymagitems") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("earlyetanks")) then
-    return true
-  elseif hasItem("varia") or (hasItem("hs_anysuit") and (hasItem("gravity") or hasItem("phazon"))) then
-    return true
-  elseif hasItem("t_earlymagitems") and (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("sb_earlyetanks")) then
-    return true, AccessibilityLevel.SequenceBreak
-  elseif hasItem("sb_t_earlymagitems") and ((Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("earlyetanks")) or (Tracker:ProviderCountForCode("etanks") >= Tracker:ProviderCountForCode("sb_earlyetanks"))) then
-    return true, AccessibilityLevel.SequenceBreak
-  else
-    return false
-  end
+function canIsg()
+  return hasItem(morph) and hasItem(bomb) and hasItem(boost)
 end
 
-function ERIB()
-  if hasItem("t_terrain") and hasItem("t_ghetto") then
-    return (hasItem("t_eliteib") and hasItem("boost")) or (hasItem("morph") and hasItem("bombs"))
-  else
-    return true
-  end
+function canWallcrawl()
+  return (canBomb() or hasItem(t_outOfBoundsWithoutMorphBall)) and hasItem(space)
 end

56a9f3a0fcd0194ecb866b6d8147dbe26018fc3d
SauceRelic <fadingraider@gmail.com>
2020-09-12 08:00:46 -0500

    rename again because i have no foresight

--- mp1rando_saucerelic/scripts/interface.lua
+++ mp1rando_saucerelic/scripts/interface.lua
@@ -2,6 +2,20 @@
 
 end--]]
 
+function 
+
+function startingAreaVis()
+  local anchorsTable = {"temple", "to_north", "to_east", "to_west", "to_southchozo", "to_southmines", "cr_west", "cr_north", "cr_east", "cr_south",
+    "mc_north", "mc_west", "mc_east", "mc_southmines", "mc_southphen", "pd_north", "pd_south", "pm_east", "pm_west"}
+  
+  for i,code in ipairs(anchorsTable) do
+    if has(code) then
+      return false
+    end
+  end
+  return true
+end
+
 function trick(code, desiredStage)
   local setting = Tracker:FindObjectForCode(code)
   if setting then

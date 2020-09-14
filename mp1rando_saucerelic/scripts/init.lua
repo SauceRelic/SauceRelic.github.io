16e93966c580ce368ef5db1f5cddb0a2acb50a91
SauceRelic <fadingraider@gmail.com>
2020-09-14 00:28:00 -0500

    add global variables, cleanup and formatting

--- mp1rando_saucerelic/scripts/init.lua
+++ mp1rando_saucerelic/scripts/init.lua
@@ -3,18 +3,19 @@ Tracker:AddItems("items/equips.json")
 Tracker:AddItems("items/artifacts.json")
 
 if not string.find(Tracker.ActiveVariantUID, "v_itemsonly") then
-  Tracker:AddLocations("locations/settings.json")
-  ScriptHost:LoadScript("scripts/logic.lua")
-  Tracker:AddLayouts("layouts/settings.json")
-  
+  diff_cStage = 0
+  startArea_en = false
+
   Tracker:AddItems("items/anchors.json")
   Tracker:AddMaps("maps/maps.json")
 
-  Tracker:AddLocations("locations/logic.json")
-  Tracker:AddLocations("locations/locations.json")
-end
+  ScriptHost:LoadScript("scripts/logic.lua")
+  ScriptHost:LoadScript("scripts/settings.lua")
 
-ScriptHost:LoadScript("scripts/settings.lua")
+  Tracker:AddLocations("locations/locations.json")
+
+  Tracker:AddLayouts("layouts/settings.json")
+end
 
 Tracker:AddLayouts("layouts/shared.json")
 Tracker:AddLayouts("layouts/tracker.json")
